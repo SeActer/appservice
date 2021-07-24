72bdf3a45ab7eb38cd0d2f3fe69c104ff33976ef
seacter <869234122@qq.com>
2021-07-24 00:46:51 -0700

    update

--- controllers/myapp_controller.go
+++ controllers/myapp_controller.go
@@ -18,7 +18,11 @@ package controllers
 
 import (
 	"context"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/util/json"
+	"k8s.io/client-go/util/retry"
+	"reflect"
 
 	"github.com/go-logr/logr"
 	appv1beta1 "github.com/seacter/appservice/api/v1beta1"
@@ -26,6 +30,10 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+var (
+	oldSpecAnnotations = "old/spec"
 )
 
 // MyAppReconciler reconciles a MyApp object
@@ -61,16 +69,34 @@ func (r *MyAppReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 
 	//如果不存在关联的资源，是不是应该去创建
 	//如果存在关联的资源，是不是要判断是否需要更新
-	deploy  := &appsv1.Deployment{}
-	if err := r.Client.Get(ctx,req.NamespacedName,deploy); err != nil && errors.IsNotFound(err) {
+	deploy := &appsv1.Deployment{}
+	if err := r.Client.Get(ctx, req.NamespacedName, deploy); err != nil && errors.IsNotFound(err) {
+		data, err := json.Marshal(myapp.Spec)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+		if myapp.Annotations != nil {
+			myapp.Annotations[oldSpecAnnotations] = string(data)
+		} else {
+			myapp.Annotations = map[string]string{
+				oldSpecAnnotations: string(data),
+			}
+		}
+		//重新更新myapp
+		if err := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+			return r.Client.Update(ctx, &myapp)
+		}); err != nil {
+			return ctrl.Result{}, err
+		}
+
 		//deployment不存在,创建deployment
 		newDeploy := NewDeploy(&myapp)
-		if err := r.Client.Create(ctx,newDeploy);err != nil{
+		if err := r.Client.Create(ctx, newDeploy); err != nil {
 			return ctrl.Result{}, err
 		}
 		//直接创建svc
 		newService := NewService(&myapp)
-		if err := r.Client.Create(ctx,newService);err != nil{
+		if err := r.Client.Create(ctx, newService); err != nil {
 			return ctrl.Result{}, err
 		}
 
@@ -79,7 +105,46 @@ func (r *MyAppReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 
 	}
 
-	//TODO 更新
+	//TODO 更新,判断是否需要更新（yaml文件是不是变化了）
+	//yaml-> old yaml 我们可以从annotations里面获取
+	oldSpec := appv1beta1.MyAppSpec{}
+	if err := json.Unmarshal([]byte(myapp.Annotations[oldSpecAnnotations]), &oldSpec); err != nil {
+		return ctrl.Result{}, err
+	}
+
+	//和新旧对象进行比较
+	if !reflect.DeepEqual(myapp.Spec, oldSpec) {
+		//更新关联资源
+		newDeploy := NewDeploy(&myapp)
+		oldDeploy := &appsv1.Deployment{}
+		if err := r.Client.Get(ctx, req.NamespacedName, oldDeploy); err != nil {
+			return ctrl.Result{}, err
+		}
+		oldDeploy.Spec = newDeploy.Spec
+		//正常直接去更新
+		//一般情况下不会直接调用update更新
+		//r.Client.Update(ctx,oldDeploy)
+		if err := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+			return r.Client.Update(ctx, oldDeploy)
+		}); err != nil {
+			return ctrl.Result{}, err
+		}
+	}
+
+	//Service
+
+	newService := NewService(&myapp)
+	oldService := &corev1.Service{}
+	if err := r.Client.Get(ctx, req.NamespacedName, oldService); err != nil {
+		return ctrl.Result{}, err
+	}
+	newService.Spec.ClusterIP = oldService.Spec.ClusterIP
+	oldService.Spec = newService.Spec
+	if err := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+		return r.Client.Update(ctx, oldService)
+	}); err != nil {
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
 }
